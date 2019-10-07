20267ad80172a38988ae8fdbe9a94e1da1d4645e
Jonathan Hellberg <jonathanh98@live.se>
2019-10-08 01:15:53 +0200

    first commit

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
+app.use(cors());
 
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
