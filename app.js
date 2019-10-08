d1c01e3776ff276cb60d36dc5a029adf54e28093
Jonathan Hellberg <jonathanh98@live.se>
2019-10-08 23:33:50 +0200

    first  commit

--- app.js
+++ app.js
@@ -7,14 +7,18 @@ app.use(cors());
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
+let ioz = io;
+let userz = {};
+
 io.origins(['https://jhellberg.me:443']);
 
 io.on('connection', function (socket) {
     console.info("User connected");
 
-    let user = "User" + this.users + "just connected";
-
-    io.emit(user);
+    socket.on('connected', (user) => {
+        userz[socket.id] = user;
+        io.emit('users', userz);
+      });
 
     socket.on('chat message', function (message) {
         io.emit('chat message', message);
